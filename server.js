cb731473cfd857b1ffe1223f8b018d5c8f30a0f1
Yen Xiong Yuan <yenyuan17@gmail.com>
2023-01-20 02:23:24 +0000

    finished lab 8

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ console.log("This is the first server");
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
+const axios = require("axios");
 
 // *** FOR LAB DON'T FORGET TO REQUIRE YOUR STARTER JSON FILE ***
 let data = require("./data/weather.json");
@@ -29,7 +30,7 @@ const PORT = process.env.PORT || 3002;
 
 // *** Callback function - 2 parameters: request, response (req,res)
 
-app.get("/weather", async (request, response, next) => {
+app.get('/weather', async (request, response, next) => {
   try {
     // let { searchQuery } = request.query;
 
@@ -39,12 +40,27 @@ app.get("/weather", async (request, response, next) => {
     let url = `http://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.REACT_APP_WEATHER_API_KEY}&lat=${lat}&lon=${lon}&days=3&units=I`;
 
      let weatherFromAxios = await axios.get(url);
-    // console.log(weatherFromAxios);
+    console.log(weatherFromAxios);
     let arrayDays = weatherFromAxios.data.data;
     let weatherData = arrayDays.map(day => new Forecast(day));
     // console.log(arrayDays);
     
     response.status(200).send(weatherData);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get("/movies", async (request, response, next) => {
+  try {
+    let searchQuery = request.query.searchQuery;
+    let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${searchQuery}&language=en-US&page=1&include_adult=false`;
+
+    let moviesFromAxios = await axios.get(url);
+    let movieArray = moviesFromAxios.data.results;
+    let movieResult = movieArray.map((movie) => new Movie(movie));
+
+    response.status(200).send(movieResult);
   } catch (error) {
     next(error);
   }
@@ -56,6 +72,13 @@ class Forecast {
   constructor(cityObj) {
     this.description = cityObj.weather.description;
     this.date = cityObj.datetime;
+  }
+}
+
+class Movie {
+  constructor(movieObj) {
+    this.movie = movieObj.title;
+    this.description = movieObj.overview;
   }
 }
 
