57816674af7310b6355fd0eab11303398dab3159
Yen Xiong Yuan <yenyuan17@gmail.com>
2023-01-21 01:06:05 +0000

    finish lab 10

--- modules/movies.js
+++ modules/movies.js
@@ -1,17 +1,52 @@
 "use strict";
 const axios = require("axios");
 
+let  cache = {};
+
+//TODO: 1. Need to create a key for the data I'm going to store
+//TODO: 2. If the thing exist AND in a valid timeframe ... send that data
+//TODO: 3. If the thing DOES NOT exist - call API and cache what is returned from my API
+
+/*
+cache = {
+  key-movies:{
+    data: from API,
+    timeStamp: Date.now()
+  }
+}
+*/
 
 async function getMovies(request, response, next) {
   try {
     let searchQuery = request.query.searchQuery;
-    let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${searchQuery}&language=en-US&page=1&include_adult=false`;
 
-    let moviesFromAxios = await axios.get(url);
-    let movieArray = moviesFromAxios.data.results;
-    let movieResult = movieArray.map((movie) => new Movie(movie));
+    // *** #1 CREATE MY KEY ***
+    let key = `${searchQuery}Movies`;  // -->> key == cityMovies     cache[cityMovies] []- use varibles 
 
-    response.status(200).send(movieResult);
+    //*** #2 IF IT EXISTS AND IT IS IN A VALID TIME - SEND THAT DATA  ***
+    if(cache[key] && (Date.now() - cache[key].timeStamp) < 10000){
+       console.log('Cache was hit, movie data is showing')
+       response.status(200).send(cache[key].data)
+    } else {
+
+          console.log('Cache missed -- NO movies data');
+
+          let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${searchQuery}&language=en-US&page=1&include_adult=false`;
+      
+          let moviesFromAxios = await axios.get(url);
+          let movieArray = moviesFromAxios.data.results;
+          let movieResult = movieArray.map((movie) => new Movie(movie));
+
+          // *** Cache the results from the API call
+
+          cache[key] = {
+            data: movieResult,
+            timeStamp: Date.now()
+          }
+          response.status(200).send(movieResult);
+    }
+
+
   } catch (error) {
     next(error);
   }
@@ -25,5 +60,3 @@ class Movie {
 }
 
 module.exports = getMovies;
-
-
--- server.js
+++ server.js
@@ -1,57 +1,30 @@
-"use strict";
+'use strict';
 
-console.log("This is the first server");
-
-// **** REQUIRES ****
-const express = require("express");
-require("dotenv").config();
-const cors = require("cors");
-const axios = require("axios");
-const getWeather = require('./modules/weather');
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const weather = require('./modules/weather.js');
 const getMovies = require('./modules/movies');
 
-
-// *** FOR LAB DON'T FORGET TO REQUIRE YOUR STARTER JSON FILE ***
-let data = require("./data/weather.json");
-
-// **** Once express is in we need to use it - per express docs
-// *** app === server
 const app = express();
 
-// **** MIDDLEWARE ****
-// *** cors is middleware - security guard that allows us to share resources across the internet ***
 app.use(cors());
 
-// *** DEFINE A PORT FOR MY SERVER TO RUN ON ***
 const PORT = process.env.PORT || 3002;
 
-// **** ENDPOINTS ****
-
-// *** Base endpoint - proof of life
-// ** 1st arg - endpoint in quotes
-// ** 2nd arg - callback which will execute when someone hits that point
-
-// *** Callback function - 2 parameters: request, response (req,res)
-
-
-
-app.get('/weather', getWeather);
-
+app.get('/weather', weatherHandler);
 app.get('/movies', getMovies);
 
+function weatherHandler(request, response) {
+  const { lat, lon } = request.query;
+  weather(lat, lon)
+    .then((summaries) => response.send(summaries))
+    .catch((error) => {
+      console.error(error);
+      response.status(200).send('Sorry. Something went wrong!');
+    });
+}
 
-
-// **** CLASS TO GROOM BULKY DATA ****
-
-// **** CATCH ALL ENDPOINT - NEEDS TO BE YOUR LAST DEFINED ENDPOINT ****
-app.get("*", (request, response) => {
-  response.status(404).send("This page does not exist");
-});
-
-// **** ERROR HANDLING - PLUG AND PLAY CODE FROM EXPRESS DOCS ****
-app.use((error, request, response, next) => {
-  response.status(500).send(error.message);
-});
-
-// ***** SERVER START ******
-app.listen(PORT, () => console.log(`Using port: ${PORT}`));
+app.listen(process.env.PORT, () =>
+  console.log(`Server up on ${process.env.PORT}`)
+);
